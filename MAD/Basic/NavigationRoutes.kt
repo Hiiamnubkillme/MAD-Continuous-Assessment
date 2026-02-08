26f15a7d4969ad06effa2e029b5498126a95dbc8
Hiiamnubkillme <109723610+Hiiamnubkillme@users.noreply.github.com>
2026-02-08 23:30:08 +0800

    Delete MAD/Basic/NavigationRoutes.kt

--- MAD/Basic/NavigationRoutes.kt
+++
@@ -1,8 +0,0 @@
-package np.ict.mad.mad_assignment
-
-object Routes {
-    const val Start = "start"
-    const val Home = "home"
-    const val AddTask = "add_task"
-    const val Details = "details/{taskId}"
-}
